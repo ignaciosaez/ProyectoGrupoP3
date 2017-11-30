efe71a4034b360497ace2c9cca207a5ad4b28f69
Paula Elosegui <paula.elosegui@opendeusto.es>
2017-11-30 11:03:47 +0100

    Comienzo de la clase gestorTrabajadores y FrameEntrarTrabajador

---
+++ Program3project/src/LP/FrameEntrarTrabajador.java
@@ -0,0 +1,31 @@
+package LP;
+
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import static COMUN.constantesActionCommand.*;
+public class FrameEntrarTrabajador extends JFrame implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
